7f2e11809a1e1a370158a7652526292cd7ed0ae7
Jerome Lin <jeromelin@axvs-MacBook-Pro-2.local>
2024-05-07 23:15:00 +0800

    router init

--- app.js
+++ app.js
@@ -6,6 +6,37 @@ app.get('/', (req, res) => {
     res.send('Express server is running.')
 })
 
+app.get('/todos', (req, res) => {
+    res.send('Get all todos')
+})
+
+app.get('/todos/:id', (req, res) => {
+    res.send(`get todo id: ${req.params.id}`)
+})
+
+app.get('/todos/new', (req, res) => {
+    res.send('Create todo')
+})
+
+
+app.post('/todos', (req, res) => {
+    res.send('add todos')
+})
+
+app.get('/todos/:id/edit', (req, res) => {
+    res.send(`edit todo id:${req.params.id}`)
+})
+
+app.put('/todos/:id', (req, res) => {
+    res.send(`update todos id:${req.params.id}`)
+})
+
+app.delete('/todos/:id', (req, res) => {
+    res.send(`delete todos id: ${req.params.id}`)
+})
+
+
+
 app.listen(port, () => {
     console.log(`express server is running on http://localhost:${port}`)
 })
