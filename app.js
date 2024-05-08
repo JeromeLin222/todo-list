d3abfe6dc63e2caced1f7d0ed8c141d8eb5070a4
Jerome Lin <jeromelin@axvs-MacBook-Pro-2.local>
2024-05-09 00:08:34 +0800

    implement creating todo

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.engine('.hbs', engine({ extname: '.hbs'}))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
     res.render('index')
 })
@@ -26,11 +28,14 @@ app.get('/todos', (req, res) => {
 })
 
 app.get('/todos/new', (req, res) => {
-    res.send('Create todo')
+    return res.render('new')
 })
 
 app.post('/todos', (req, res) => {
-    res.send('add todos')
+    const name = req.body.name
+    return Todo.create({ name })
+        .then(() => res.redirect('/todos'))
+        .catch((err) => console.lof(err))
 })
 
 app.get('/todos/:id', (req, res) => {
