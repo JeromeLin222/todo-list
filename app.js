b56c698506b54f8531da44cc3c284d1268542052
Jerome Lin <jeromelin@axvs-MacBook-Pro-2.local>
2024-05-09 18:14:29 +0800

    implement updating todo

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 
 const { engine } = require('express-handlebars')
+const methodOverride = require('method-override')
 
 const db = require('./models')
 const Todo = db.Todo
@@ -13,6 +14,7 @@ app.set('view engine', '.hbs')
 app.set('views', './views')
 
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     res.render('index')
@@ -44,17 +46,29 @@ app.get('/todos/:id', (req, res) => {
         attributes: ['id', 'name'],
         raw: true
     })
-    .then((todo) => res.render('todo', { todo }))
-    .catch((err) => console.log(err))
+        .then((todo) => res.render('todo', { todo }))
+        .catch((err) => console.log(err))
 })
 
 
 app.get('/todos/:id/edit', (req, res) => {
-    res.send(`edit todo id:${req.params.id}`)
+    const id = req.params.id
+    return Todo.findByPk(id, {
+        attributes: ['id', 'name'],
+        raw: true
+    })
+        .then((todo) => res.render('edit', { todo }))
 })
 
 app.put('/todos/:id', (req, res) => {
-    res.send(`update todos id:${req.params.id}`)
+    const body = req.body
+    const id = req.params.id
+    return Todo.update(
+        { name: body.name },
+        { where: {id: id}}
+    )
+        .then(() => res.redirect(`/todos/${id}`))
+
 })
 
 app.delete('/todos/:id', (req, res) => {
